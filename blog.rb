659dd2998e0e73ade04e4ebc093f1616c31c0c06
Geoffrey Litt <gklitt@gmail.com>
2020-07-19 11:51:22 -0400

    improve start script

--- blog.rb
+++ blog.rb
@@ -1,8 +1,19 @@
+#!/usr/bin/ruby
+
+# Creates a new blog post, and opens an editor to the new file
+
+# Lol, just join all the args into the title...
+# can type ./blog.rb This is the Title
 title = ARGV[0]
 Dir.chdir "/Users/geoffreylitt/dev/homepage" do
   puts "Creating blog post: #{title}..."
-  created = `bundle exec middleman article #{title}`
+  created = `bash -l -c "bundle exec middleman article '#{title}'"`
   puts created
   filename = created.split(" ")[1]
-  `code #{filename}`
+
+  # open the whole site in vscode
+  `bash -l -c "code ."`
+
+  # open the new blog post
+  `bash -l -c "code #{filename}"`
 end
